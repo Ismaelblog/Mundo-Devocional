f9967fe9e55ca3e3cb4a3bbf804765816df53d4f
Ismaelblog <ismaelblog@hotmail.com>
2024-09-06 15:29:56 -0300

    Projeto-Atualizado
    Projeto do site-web Mundo Devocional foi atualizado pelo final do evento da imersão Alura+Google Gemini, na data de 06/09/24.

--- app.js
+++ app.js
@@ -2,14 +2,33 @@ function pesquisar() {
     // Função para realizar uma pesquisa e exibir os resultados na página.
   
     // Seleciona a seção onde os resultados serão exibidos.
-    let section = document.getElementById("resultados-pesquisa");
-  
+    let section = document.getElementById("resultados-pesquisa")
+    
+    let campoPesquisa = document.getElementById("campo-pesquisa").value
+
+    // se campoPesquisa for uma string sem nada:
+    if (campoPesquisa == "") {
+      section.innerHTML = "Nenhum Devocional Encontrado."
+        return
+    }
+
+    campoPesquisa = campoPesquisa.toLowerCase()
+    
     // Inicializa uma string vazia para armazenar os resultados formatados.
     let resultados = "";
+    let titulo = "";
+    let descrição = "";
+    let tags = "";
   
     // Itera sobre cada dado na lista de dados.
     for (let dado of dados) {
-      // Cria uma nova div para cada resultado, formatando os dados do objeto.
+      titulo = dado.titulo.toLowerCase()
+      descricao = dado.descrição.toLowerCase()
+      tags = dado.tags.toLowerCase()
+
+      // se o titulo includes campoPesquisa
+      if (titulo.includes(campoPesquisa) || descrição.includes(campoPesquisa) || tags.includes(campoPesquisa))  {
+      //cria um novo elemento
       resultados += `
         <div class="item-resultado">
           <h2>
@@ -19,8 +38,13 @@ function pesquisar() {
           <a href="${dado.link}" target="_blank">Adquira o Livro</a>
         </div>
       `;
-    }
-  
+      }
+
+      if (!resultados) {
+          resultados = "Nenhum Devocional Encontrado."
+      }
+
     // Atribui a string com os resultados ao conteúdo da seção.
     section.innerHTML = resultados;
   }
+}
--- dados.js
+++ dados.js
@@ -2,16 +2,19 @@ let dados = [
     {
     titulo: "Perseverança",
     descrição: "Através de reflexões profundas sobre passagens bíblicas como a de Jó, que suportou perdas inimagináveis, ou a de Paulo, que enfrentou perseguições implacáveis, o devocional pode fortalecer a convicção de que a perseverança não é apenas uma virtude, mas um dom divino que nos capacita a superar qualquer obstáculo.",
-    link: "https://publicacoespaodiario.com.br/produto/pao-diario-perseveranca-e-oracao/"
+    link: "https://publicacoespaodiario.com.br/produto/pao-diario-perseveranca-e-oracao/",
+    tags: "perseverar jó fé confiança"
     },
   {
     titulo: "Gratidão",
     descrição: "Cultivar a gratidão transforma a nossa perspectiva de vida, permitindo que enxerguemos as bênçãos presentes em cada dia. Este devocional nos convida a expressar gratidão por tudo o que temos, fortalecendo nossa fé e aproximando-nos de Deus.",
-    link: "https://publicacoespaodiario.com.br/produto/pao-diario-alegria-e-gratidao/"
+    link: "https://publicacoespaodiario.com.br/produto/pao-diario-alegria-e-gratidao/",
+    tags: "grato gratificante fé"
   },
  {
     titulo: "Fé",
     descrição: "A fé é um dom precioso que nos conecta com o divino e nos guia em meio às incertezas da vida. Este devocional explora a natureza da fé, inspirando-nos a confiar em Deus em todas as circunstâncias.",
-    link: "https://publicacoespaodiario.com.br/produto/inspiracoes-de-fe/"
+    link: "https://publicacoespaodiario.com.br/produto/inspiracoes-de-fe/",
+    tags: "fé luta confiança"
   }
 ]
